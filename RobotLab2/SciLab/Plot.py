ed751df907c1a46cfcccb0bbbaec26433c1a0b81
Georgy Khachatryan <AAC-Gudini@yandex.ru>
2020-10-28 17:06:19 +0300

    Omega of t plot

--- RobotLab2/SciLab/Plot.py
+++ RobotLab2/SciLab/Plot.py
@@ -14,20 +14,22 @@ def ReadFile(name):
             break;
     return [values, time]
 
-colors = ['r', '--b']
+colors = ['r', 'g', '--b']
 
-for idx in range(-10, 0):
+for idx in range(1, 10, 2):
     data = ReadFile('../Data/log' + str(idx * 10) + '.txt')
-    sim = ReadFile('../Sim/Sim' + str(idx * 10) + '.txt')
+    theta = ReadFile('../Sim/ThetaSim' + str(idx * 10) + '.txt')
+    thetaDot = ReadFile('../Sim/ThetaDotSim' + str(idx * 10) + '.txt')
 
     data[0] = [i * 0.017453 for i in data[0]]
 
 
-    plt.plot(sim[1], sim[0], colors[0], alpha=1.0, linewidth=1)
-    plt.plot(data[1], data[0], colors[1], alpha=0.8, linewidth=1)
+    plt.plot(theta[1], theta[0], colors[0], alpha=1.0, linewidth=1, label=('θ(t)' if idx == 1 else ''))
+    plt.plot(thetaDot[1], thetaDot[0], colors[1], alpha=0.9, linewidth=1, label=('ω(t)' if idx == 1 else ''))
+    plt.plot(data[1], data[0], colors[2], alpha=0.8, linewidth=1, label=('θ(t) - measured' if idx == 1 else ''))
 
 plt.minorticks_on()
-plt.grid(which="major", color="k", linewidth=0.5)
-plt.grid(which="minor", color="k", linestyle=":", linewidth=0.5)
-
+plt.grid(which="major", color="k", linewidth=0.5, alpha=0.5)
+plt.grid(which="minor", color="k", linestyle=":", linewidth=0.5, alpha=0.5)
+plt.legend()
 plt.show()
