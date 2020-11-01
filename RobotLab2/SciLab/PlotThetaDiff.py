683803b8a5c62f23707a2dc08d7abeb09ce297a9
Georgy Khachatryan <aac-gudini@yandex.ru>
2020-11-01 20:55:47 +0300

    Theta of t diff

---
+++ RobotLab2/SciLab/PlotThetaDiff.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+
+plt.style.use('fast')
+
+def ReadFile(name, cols=2):
+    f = open(name, 'r')
+    ret = []
+    for i in range(cols):
+        ret.append([])
+    for line in f:
+        v = [float(i) for i in line.split()]
+        for i in range(cols):
+            ret[i].append(v[i])
+        if ret[1][-1] > 0.05:
+            break;
+    return ret
+
+colors = ['r', 'g', '--b']
+
+for idx in range(1, 10, 2):
+    simple = ReadFile('../Sim/Theta_simply_Sim' + str(idx * 10) + '.txt')
+    full   = ReadFile('../Sim/ThetaSim' + str(idx * 10) + '.txt')
+    data   = ReadFile('../Sim/ThetaSim' + str(idx * 10) + '.txt')
+
+    plt.plot(full[1], full[0], colors[0], alpha=0.9, linewidth=1, label=('θ(t) - full' if idx == 1 else ''))
+    plt.plot(simple[1], simple[0], colors[1], alpha=0.9, linewidth=1, label=('θ(t) - simple' if idx == 1 else ''))
+    plt.plot(data[1], data[0], colors[2], alpha=0.9, linewidth=1, label=('θ(t) - data' if idx == 1 else ''))
+
+plt.minorticks_on()
+plt.grid(which="major", color="k", linewidth=0.5, alpha=0.5)
+plt.grid(which="minor", color="k", linestyle=":", linewidth=0.5, alpha=0.5)
+plt.legend()
+plt.show()
