c8e5116b03f34294b80768a74eba60dc3b2349e0
Georgy Khachatryan <AAC-Gudini@yandex.ru>
2021-03-09 11:59:57 +0300

    Back to the working state

--- RobotLab4/src/main.py
+++ RobotLab4/src/main.py
@@ -6,7 +6,7 @@ import math
 import time
 
 # Clamps value in the [min, max] range
-def clamp(val: float, _min: float, _max: float)-> float:
+def clamp(val, _min, _max):
     if val < _min:
         return _min
     if (val > _max):
@@ -14,81 +14,108 @@ def clamp(val: float, _min: float, _max: float)-> float:
     return val
 
 # Adds +- 2Pi if the difference is larger then Pi
-def unwrap(val: float, previous_val: float)-> float:
-    difference = previous_val - val
-    while difference > math.pi:
-        val        += math.pi * 2.
-        difference -= math.pi * 2.
+def unwrap(val, previous_val):
+    # difference = previous_val - val
+    # while difference > math.pi:
+    #     val        += math.pi * 2.
+    #     difference -= math.pi * 2.
+    #     
+    # while difference < -math.pi:
+    #     val        -= math.pi * 2.
+    #     difference += math.pi * 2.
         
-    while difference < -math.pi:
-        val        -= math.pi * 2.
-        difference += math.pi * 2.
-        
+    return val
+
+def threshold_clamp(val, _min, _max, threshold_min, threshold_max):
+    val = clamp(val, _min, _max)
+    if val > threshold_min and val < threshold_max:
+        return threshold_max    
+    if val < -threshold_min and val > -threshold_max:
+        return -threshold_max
+    
+    if val < threshold_min and val > -threshold_min:
+        return 0    
+    
     return val
 
 motor_a = LargeMotor('outA')
 motor_d = LargeMotor('outD')
 
-inv_wheel_span: float = 1. / 0.16
-wheel_radius  : float = 0.056
+inv_wheel_span = 1. / 0.17
+wheel_radius = 0.028
 
-linear    : float = 5
-rotational: float = 10
+linear     = 75
+rotational = -200
 
 motor_a.position = 0
 motor_d.position = 0
 
-x_goal: float = 2.
-y_goal: float = 2.
+x_goal = 1.
+y_goal = 0.5
 
-x_current: float = 0.
-y_current: float = 0.
+x_current = 0.
+y_current = 0.
 
-theta  : float = 0.
-azimuth: float = 0.
+theta   = 0.
+azimuth = 0.
 
-left_angle : float = 0.
-right_angle: float = 0.
+left_angle  = 0.
+right_angle = 0.
 
-dt    : float = 1.
-inv_dt: float = 1.
+dt     = 1.
+inv_dt = 1.
 
 time_start = time.time()
-while abs(x_current - x_goal) > 0.1 and abs(y_current - x_goal) > 0.1:
-    x_vec: float = x_goal - x_current
-    y_vec: float = y_goal - y_current
+while (abs(x_current - x_goal) > 0.1) or (abs(y_current - y_goal) > 0.1):
+    motor_a.position = 0
+    motor_d.position = 0
     
-    u_linear: float = math.sqrt(x_vec ** 2 + y_vec ** 2) * linear
-    u_linear: float = clamp(u_linear, -100, 100)
+    x_vec = x_goal - x_current
+    y_vec = y_goal - y_current
+    
+    u_linear = math.sqrt(x_vec ** 2 + y_vec ** 2) * linear
+    u_linear = clamp(u_linear, -100, 100)
     
     azimuth = unwrap(math.atan2(y_vec, x_vec), azimuth)
     
-    u_rotational: float = (azimuth - theta) * rotational
-    u_rotational: float = clamp(u_rotational, -100, 100)
+    # print("azimuth: {0}, theta: {1}".format(azimuth, theta))
     
-    u_left : float = clamp(u_linear - u_rotational, -100, 100)
-    u_right: float = clamp(u_linear + u_rotational, -100, 100)
+    u_rotational = (azimuth - theta) * rotational
+    u_rotational = clamp(u_rotational, -100, 100)
     
-    new_left_angle : float = motor_a.position * +0.0174532925199432 
-    new_right_angle: float = motor_d.position * -0.0174532925199432  # TODO: Check this minus sign
+    u_left  = clamp(u_linear - u_rotational, -100, 100)
+    u_right = clamp(u_linear + u_rotational, -100, 100)
     
-    left_speed : float = (new_left_angle  - left_angle)  * inv_dt # TODO: Probably this dt's are not needed because they are 
-    right_speed: float = (new_right_angle - right_angle) * inv_dt # annihilated by the dt's when we are integrating position
+    # Large motor has 720 pulses per revolutions as stated in https://github.com/ev3dev/ev3dev/issues/148#issuecomment-202498715
+    new_left_angle  = motor_a.position * 4 * 0.0174532925199432 
+    new_right_angle = motor_d.position * 4 * 0.0174532925199432 # TODO: Check this minus sign
     
-    left_angle  = new_left_angle
-    right_angle = new_right_angle
+    # print("motor_a: {0}, motor_d: {1}".format(motor_a.position, motor_d.position))
     
-    theta        = (left_angle + right_angle) * wheel_radius * inv_wheel_span
-    speed: float = (left_speed + right_speed) * wheel_radius * 0.5
+    # left_speed  = (new_left_angle  - left_angle)  * inv_dt # TODO: Probably this dt's are not needed because they are 
+    # right_speed = (new_right_angle - right_angle) * inv_dt # annihilated by the dt's when we are integrating position
+    
+    left_speed  = new_left_angle  * inv_dt
+    right_speed = new_right_angle * inv_dt
+    
+    left_angle  += new_left_angle
+    right_angle += new_right_angle
+    
+    theta = (left_angle - right_angle) * wheel_radius * inv_wheel_span
+    speed = (left_speed + right_speed) * wheel_radius * 0.5
 
-    x_speed: float = math.cos(theta) * speed
-    y_speed: float = math.sin(theta) * speed
+    x_speed = math.cos(theta) * speed
+    y_speed = math.sin(theta) * speed
     
     x_current += x_speed * dt # We are integrating position right here
     y_current += y_speed * dt # TODO: Delete this dt's
     
-    motor_a.run_direct(duty_cycle_sp=int(u_left))
-    motor_d.run_direct(duty_cycle_sp=int(u_right))
+    # print("x_speed: {0}, y_speed: {1}".format(x_speed, y_speed))
+    # print("x_current: {0}, y_current: {1}, azimuth: {2}".format(x_current, y_current, azimuth))
+    print("x_current: {0}, y_current: {1}, theta: {2}".format(x_current, y_current, theta))
+    
+    motor_a.run_direct(duty_cycle_sp=int(threshold_clamp(u_left, -100, 100, 5, 45)))
+    motor_d.run_direct(duty_cycle_sp=int(threshold_clamp(u_right, -100, 100, 5, 45)))
     
     dt     = time_start - time.time()
     inv_dt = 1. / dt
---
+++ RobotLab4/src/square.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+
+from ev3dev.ev3 import *
+import math
+import time
+
+# Clamps value in the [min, max] range
+def clamp(val: float, _min: float, _max: float)-> float:
+    if val < _min:
+        return _min
+    if (val > _max):
+        return _max
+    return val
+
+# Adds +- 2Pi if the difference is larger then Pi
+def unwrap(val: float, previous_val: float)-> float:
+    difference = previous_val - val
+    while difference > math.pi:
+        val        += math.pi * 2.
+        difference -= math.pi * 2.
+        
+    while difference < -math.pi:
+        val        -= math.pi * 2.
+        difference += math.pi * 2.
+        
+    return val
+
+motor_a = LargeMotor('outA')
+motor_d = LargeMotor('outD')
+
+inv_wheel_span: float = 1. / 0.16
+wheel_radius  : float = 0.056
+
+linear    : float = 5
+rotational: float = 10
+
+motor_a.position = 0
+motor_d.position = 0
+
+x_goal: float = 2.
+y_goal: float = 2.
+
+x_current: float = 0.
+y_current: float = 0.
+
+theta  : float = 0.
+azimuth: float = 0.
+
+left_angle : float = 0.
+right_angle: float = 0.
+
+dt    : float = 1.
+inv_dt: float = 1.
+
+time_start = time.time()
+
+points = [(1, 1), (-1, 1), (-1, -1), (1, -1)]
+
+for point in points:
+    x_goal = point[0]
+    y_goal = point[1]
+    
+    while abs(x_current - x_goal) > 0.1 and abs(y_current - x_goal) > 0.1:
+        x_vec: float = x_goal - x_current
+        y_vec: float = y_goal - y_current
+        
+        u_linear: float = math.sqrt(x_vec ** 2 + y_vec ** 2) * linear
+        u_linear: float = clamp(u_linear, -100, 100)
+        
+        azimuth = unwrap(math.atan2(y_vec, x_vec), azimuth)
+        
+        u_rotational: float = (azimuth - theta) * rotational
+        u_rotational: float = clamp(u_rotational, -100, 100)
+        
+        u_left : float = clamp(u_linear - u_rotational, -100, 100)
+        u_right: float = clamp(u_linear + u_rotational, -100, 100)
+        
+        new_left_angle : float = motor_a.position * +0.0174532925199432 
+        new_right_angle: float = motor_d.position * -0.0174532925199432 # TODO: Check this minus sign
+        
+        left_speed : float = (new_left_angle  - left_angle)  * inv_dt # TODO: Probably this dt's are not needed because they are 
+        right_speed: float = (new_right_angle - right_angle) * inv_dt # annihilated by the dt's when we are integrating position
+        
+        left_angle  = new_left_angle
+        right_angle = new_right_angle
+        
+        theta        = (left_angle + right_angle) * wheel_radius * inv_wheel_span
+        speed: float = (left_speed + right_speed) * wheel_radius * 0.5
+    
+        x_speed: float = math.cos(theta) * speed
+        y_speed: float = math.sin(theta) * speed
+        
+        x_current += x_speed * dt # We are integrating position right here
+        y_current += y_speed * dt # TODO: Delete this dt's
+        
+        motor_a.run_direct(duty_cycle_sp=int(u_left))
+        motor_d.run_direct(duty_cycle_sp=int(u_right))
+        
+        dt     = time_start - time.time()
+        inv_dt = 1. / dt
+        
+        time_start = time.time()
+
+motor_a.run_direct(duty_cycle_sp=0)
+motor_d.run_direct(duty_cycle_sp=0)
