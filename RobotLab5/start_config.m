777f0884a098c4f21d044985c25a8be276334116
DmitryIo <41271764+DmitryIo@users.noreply.github.com>
2021-05-17 22:55:34 +0300

    graphs

--- RobotLab5/start_config.m
+++ RobotLab5/start_config.m
@@ -3,6 +3,6 @@ Psi0 = pi/12;                   % Initial body angle [Rad]
 Psi_d0 = 0;                     % Initial body speed [Rad/sec]
 Theta_d0 = 0;                   % Initial wheel speed [Rad/sec]
 %% K parameters
-Tp = 0.4;                       % Real transient time [sec]
+Tp = 0.45;                       % Real transient time [sec]
 Tp_z = 6.3;                     % Theoretical transient time  (n=3, Tp_z=6.3) [sec]
-K_mode = 0;                     % K mode: 0-hand mode, 1- Ackerman mode, other - auto mode   
+K_mode = 2;                     % K mode: 0-hand mode, 1- Ackerman mode, other - auto mode   
